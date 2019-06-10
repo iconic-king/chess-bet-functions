f4ca5c8e8d2937621850b5c0526b25e027c35038
Collins Muthinja <collinsmuthinja@gmail.com>
2019-06-11 00:47:49 +0300

    Welcomeemail trigger when new user enters system

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
 import * as functions from 'firebase-functions';
-
+const nodemailer = require("nodemailer");
 /**
  * Author : Collins Magondu
  */
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hi from collins!");
+async function sendMail(email:string,subject:string,body:string){
+//  const testAccount = await nodemailer.createTestAccount();
+   const name = email.split('@')[0];
+
+ const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type : "oauth2",
+        user: functions.config().gmailservice.user,
+        clientId: functions.config().gmailservice.clientid,
+        clientSecret: functions.config().gmailservice.clientsecret,
+        refreshToken: functions.config().gmailservice.refreshtoken,
+        accessToken:functions.config().gmailservice.accesstoken
+    }
+ });
+
+ const info = await transporter.sendMail({
+    from: '"Chess Bet <chessbetdevgmail.com>"',
+    to: `${name}, ${email}`,
+    subject: subject,
+    text: body
+ })
+
+console.log("Message sent : " + info.messageId);
+}
+
+
+export const sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+    const strUser= JSON.stringify(user.email);
+    sendMail(strUser,"Hello","Welcome to chess bet").catch(console.error);
 });
+
