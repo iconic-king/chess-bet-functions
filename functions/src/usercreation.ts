5e383ffd89cebf1522d80445bbb5c37c350c0404
Collins Muthinja <collinsmuthinja@gmail.com>
2019-06-13 22:53:52 +0300

    Update dependencies]

--- functions/src/usercreation.ts
+++ functions/src/usercreation.ts
@@ -5,14 +5,17 @@ admin.initializeApp();
 const firestoreDatabase = admin.firestore();
 
 const createUserAccount =  (uid:string) => {
-  const date = new Date().toLocaleTimeString();
+  const date = new Date().toLocaleString();
 
-  const documentReference = firestoreDatabase.collection("accounts").add({
+  firestoreDatabase.collection("accounts").add({
   date_created: date,
   last_date_modified: date,
   terms_condition_accepted: false,
   events:{
-    "CREATED" : date
+    "CREATED" : {
+      date_created: date,
+      done: true
+    }
   },
   status : "PENDING",
   amount : 0,
@@ -23,19 +26,11 @@ const createUserAccount =  (uid:string) => {
   }).catch((error)=>{
     console.log(error.message)
   })
-
-  firestoreDatabase.collection("users").doc(uid).update({
-    account_ref : documentReference  
-  }).then(()=>{
-    console.log("User References Account");
-  }).catch((error)=>{
-    console.log(error.message)
-  })
 }
 
 // Initialize user and user account in firestore
 export const createUser = (user:admin.auth.UserRecord)=>{
- const date = new Date().toDateString();
+ const date = new Date().toLocaleString();
  firestoreDatabase.collection("users").doc(user.uid).set({
    email: user.email,
    uid: user.uid,
